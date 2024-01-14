9fb44ec076e6337489966aaea95c299ecf84afc1
Menna Hassan <h.menna85@gmail.com>
2024-01-14 06:10:37 +0200

    handled api status by changing cors, adding api_status element to be aaccessed by HTML, CSS and JS

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -4,6 +4,9 @@
  *   - if checkbox is checked, it store amenity id in a variable(dict or list)
  *   - if checkbox is unchecked, it remove amenity id from the variable.
  *   - update h4 tag inside div amenities with list of amenities checked
+ * Request http://0.0.0.0:5001/api/v1/status/:
+ *   - If in the status is “OK”, add the class available to the div#api_status
+ *   - Otherwise, remove the class available to the div#api_status
  */
 
 $(document).ready(function () {
@@ -13,6 +16,18 @@ $(document).ready(function () {
     // update the h4 tag with selected amenities
     const amenitiesList = Object.values(selectedAmenities).join(', ');
     $('div.amenities h4').text(amenitiesList);
+  }
+
+  function checkAPI() {
+    // checks api status if 'OK'
+    $.get('http://localhost:5001/api/v1/status/', function (data, status) {
+      if (data.status == 'OK') {
+        $('div#api_status').addClass('available');
+      }
+      else {
+        $('div#api_status').removeClass('available')
+      }
+    });
   }
 
   // Listen for changes on each input checkbox tag
@@ -29,5 +44,6 @@ $(document).ready(function () {
     }
 
     updateAmenitiesList();
+    checkAPI();
   });
 });
